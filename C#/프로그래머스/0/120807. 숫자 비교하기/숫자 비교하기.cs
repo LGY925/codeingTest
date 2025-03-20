468c927ec697b0692f4a0aa443672da6db1eda63
이규영 <olo66499133@gmail.com>
2025-03-20 16:55:32 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.23 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int num1, int num2) 
+    {
+        int answer;
+        int sun = (num1 - num2);
+
+        if (sun == 0 )
+        {
+            return answer = 1;
+        }
+        else
+        {
+            
+            return answer = -1;
+        }
+        
+    }
+}
