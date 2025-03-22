722160fcf77f9e609c6053852ef018d6faa239ad
이규영 <olo66499133@gmail.com>
2025-03-22 10:12:51 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.49 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution 
+{
+    public int[] solution(int[] num_list) 
+        {
+            Array.Reverse(num_list);
+        
+            int[] answer = num_list;
+            return answer;
+
+        }   
+    
+}
